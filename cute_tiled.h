1f42bb0e3740e727292b2db848fad19c1c45b6b2
Lucas_hgs <lucas.henrique.gois.santos@gmail.com>
2026-05-06 20:53:49 -0300

    tiled: Added a missing alias definition for cute_tiled_chunk_t
    Cute_tiled crashed when attempting to build  for c99 due to a typedef missing for the cute_tiled_chunk_t struct

--- cute_tiled.h
+++ cute_tiled.h
@@ -205,6 +205,7 @@ typedef struct cute_tiled_object_t cute_tiled_object_t;
 typedef struct cute_tiled_frame_t cute_tiled_frame_t;
 typedef struct cute_tiled_tile_descriptor_t cute_tiled_tile_descriptor_t;
 typedef struct cute_tiled_property_t cute_tiled_property_t;
+typedef struct cute_tiled_chunk_t cute_tiled_chunk_t;
 typedef union cute_tiled_string_t cute_tiled_string_t;
 
 /*!
